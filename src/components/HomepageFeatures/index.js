883f0ae266a6ae75d8d91bebe13f05b3593aeab3
tza <bienlor@163.com>
2024-03-01 21:50:28 +0800

    fix course progress

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: '《量子计算》课程介绍',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/QC.svg').default,
     description: (
       <>《量子计算》是一门旨在介绍量子计算原理、算法和应用的本科课程。本课程将引导学生探索量子力学的基础知识，并将其应用于计算机科学领域。学生将了解量子比特和量子门的概念，探讨量子算法的设计和分析，以及量子计算在密码学、优化和模拟等领域的潜在应用。</>
       
@@ -21,16 +21,16 @@ const FeatureList = [
   //     </>
   //   ),
   // },
-  {
-    title: '2023年《量子计算》教学成果',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+  // {
+  //   title: '2023年《量子计算》教学成果',
+  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+  //   description: (
+  //     <>
+  //       Extend or customize your website layout by reusing React. Docusaurus can
+  //       be extended while reusing the same header and footer.
+  //     </>
+  //   ),
+  // },
 ];
 
 function Feature1({Svg, title, description}) {
